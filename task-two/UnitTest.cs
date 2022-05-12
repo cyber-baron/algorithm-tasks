ffc2e7dfd06b19171e97280d8c8eded81972209b
Nadia Nikolaeva <44nikolaeva@mail.ru>
2022-05-12 23:26:27 +0300

    added sketch of the unit test

---
+++ task-two/UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            //public int[][] SortAndMerge(int[][] intervals)
+            int[][] intervals = new int[][]
+            {
+                new int[] { 11, 12 },
+                new int[] { 2, 3 },
+                new int[] { 5, 7 },
+                new int[] { 1, 4 },
+                new int[] { 8, 10 },
+                new int[] { 6, 8 }
+            };
+        }
+    }
+}
