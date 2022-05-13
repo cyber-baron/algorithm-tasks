c8337add567165d095bfcf45bd7bf5417c222bd0
Nadia Nikolaeva <44nikolaeva@mail.ru>
2022-05-13 04:01:01 +0300

    now I have only a method, deleted main lol

--- task-two/main.cs
+++ task-two/main.cs
@@ -6,25 +6,6 @@ namespace Merge_Intervals
 {
     class Solution
     {
-        static void Main(string[] args)
-        {
-            int[][] intervals = new int[][] 
-            { 
-                new int[] { 11, 12 },
-                new int[] { 2, 3 },
-                new int[] { 5, 7 }, 
-                new int[] { 1, 4 },
-                new int[] { 8, 10 },
-                new int[] { 6, 8 }
-            };
-                                             
-            var result = SortAndMerge(intervals);
-            foreach(var res in result)
-            {
-                Console.WriteLine("[" + res[0] + " , " + res[1] + "]");
-            }
-        }
-
         static int[][] SortAndMerge(int[][] intervals)
         {
             Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
